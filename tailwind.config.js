a030684a5e3be7efe1197d368bdfbe75351aaef8
Meschac Irung <meschacirung@gmail.com>
2021-12-30 18:20:02 +0200

    #7 build flat cards features block

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
 
       black: colors.black,
       white: colors.white,
-      gray: colors.coolGray,
+      gray: colors.blueGray,
       red: colors.red,
       yellow: colors.amber,
       green: colors.emerald,
