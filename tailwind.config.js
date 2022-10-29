83fea29d155b0efbb5cf32c3921633afcbcbdb52
Meschac Irung <meschacirung@gmail.com>
2022-10-29 13:30:31 +0200

    build theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
     'border-yellow-300'
   ],
   presets: [],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     screens: {
       sm: "640px",
